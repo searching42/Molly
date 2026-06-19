9c4fc14d38b6c43b1473295800f14e253a006b43
LBH <lbh42@mail.ustc.edu.cn>
2026-06-19 11:00:17 +0800

    feat: install execution confirmation audit

--- src/ai4s_agent/__init__.py
+++ src/ai4s_agent/__init__.py
@@ -1,6 +1,8 @@
 __all__ = ["__version__"]
 __version__ = "0.1.0"
 
+from ai4s_agent.execution_confirmation import install_execution_confirmation_audit
 from ai4s_agent.snapshot_material import install_run_plan_executor_snapshot_builder
 
 install_run_plan_executor_snapshot_builder()
+install_execution_confirmation_audit()
