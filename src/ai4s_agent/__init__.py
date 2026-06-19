c1625456ea611c95318f0d65c6ac4fb0731e0577
LBH <lbh42@mail.ustc.edu.cn>
2026-06-19 22:00:48 +0800

    feat: install project memory permission routes

--- src/ai4s_agent/__init__.py
+++ src/ai4s_agent/__init__.py
@@ -11,6 +11,7 @@ from ai4s_agent.external_approvals import install_external_approval_split
 from ai4s_agent.json_rmw_lock import install_json_rmw_locks
 from ai4s_agent.phase3_executor import install_phase3_executor_support
 from ai4s_agent.project_job_routes import install_project_scoped_job_routes
+from ai4s_agent.project_memory_permissions import install_project_memory_permission_routes
 from ai4s_agent.project_plan_guard import install_project_plan_route_guard
 from ai4s_agent.project_plan_routes import install_project_scoped_plan_routes
 from ai4s_agent.project_scoped_jobs import install_project_scoped_jobs
@@ -34,3 +35,4 @@ install_project_scoped_job_routes()
 install_project_scoped_plan_routes()
 install_immutable_upload_assets()
 install_server_permission_routes()
+install_project_memory_permission_routes()
