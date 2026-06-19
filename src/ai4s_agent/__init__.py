0f879c8a8d7a4bab1708832eb54e1a52e75a2eec
LBH <lbh42@mail.ustc.edu.cn>
2026-06-19 13:13:21 +0800

    feat: install execution policy registry

--- src/ai4s_agent/__init__.py
+++ src/ai4s_agent/__init__.py
@@ -4,6 +4,7 @@ __version__ = "0.1.0"
 from ai4s_agent.chat_context import install_chat_project_context
 from ai4s_agent.chat_runplan import install_chat_run_plan_routes
 from ai4s_agent.execution_confirmation import install_execution_confirmation_audit
+from ai4s_agent.execution_policy import install_execution_policy_registry
 from ai4s_agent.phase3_executor import install_phase3_executor_support
 from ai4s_agent.snapshot_material import install_run_plan_executor_snapshot_builder
 
@@ -12,3 +13,4 @@ install_execution_confirmation_audit()
 install_chat_project_context()
 install_chat_run_plan_routes()
 install_phase3_executor_support()
+install_execution_policy_registry()
